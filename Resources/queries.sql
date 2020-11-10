5fdfb8abd08d8fbc3b75b0e9350f1227a6f75b80
rvaralla1 <rvaralla1@gmail.com>
2020-11-10 18:56:51 -0500

    final push

--- Resources/queries.sql
+++ Resources/queries.sql
@@ -14,16 +14,30 @@ from departments as dep
 inner join dept_manager as dm on dep.dept_no=dm.dept_no
 inner join employees as emp on emp.emp_no=dm.emp_no
 
-4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+--4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+select emp.emp_no, emp.last_name, emp.first_name, dept_emp.dept_no
+from employees as emp
+inner join dept_emp on emp.emp_no=dept_emp.emp_no
 
 --5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 select first_name, last_name
 from employees
 where first_name = 'Hercules' and last_name like 'B%'
 
-6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+--6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+select emp.emp_no, emp.last_name, emp.first_name, dp.dept_name
+from employees as emp
+inner join dept_emp on emp.emp_no=dept_emp.emp_no
+inner join departments as dp on dept_emp.dept_no=dp.dept_no
+where dept_name = 'Sales' 
 
-7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+--7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+select emp.emp_no, emp.last_name, emp.first_name, dp.dept_name
+from employees as emp
+inner join dept_emp on emp.emp_no=dept_emp.emp_no
+inner join departments as dp on dept_emp.dept_no=dp.dept_no
+where dept_name = 'Sales' or dept_name ='Development'
+
 
 --8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 select last_name, count(last_name)
